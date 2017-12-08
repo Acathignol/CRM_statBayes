5b25516607b04408991fdea1b5f4f3e00443ee87
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2017-12-08 16:01:18 +0100

    Seance 08-12

--- main_project_FS_5BIM.R
+++ main_project_FS_5BIM.R
@@ -122,8 +122,6 @@ dose_to_give
 
 ##### Question 1.b
 
-# prior ptox
-
 d0 <- list( a0 = 1,
             dose = log(pprior_dose/(1 - pprior_dose)) - a0,
             nb_ind = 3,
@@ -136,11 +134,100 @@ model0 <- jags.model(file = textConnection(modelproject), data= d0,
 update(model0, 3000)
 mcmc0 <- coda.samples(model0, c("theta"), n.iter = 2000 )
 mcmctot0 <- as.data.frame(as.matrix(mcmc0))
+plot(mcmc0,trace=F)
 
 ptox0 <- lapply(X = dose_star_tot, FUN = function(X) {exp(a0 + mcmctot0$theta*X)/(1 +  exp(a0 + mcmctot0$theta*X))})
 ptox0 <- as.data.frame(ptox0)
 
 for(p in 1:5){
-  hist(ptox0[,p],main=paste("Dose :",as.character(dose_tot[p])))
-  abline(v=pprior_dose_tot[i],col="red")
+  hist(ptox0[,p],main=paste("Dose :",as.character(dose_tot[p])),breaks=30)
+  abline(v=pprior_dose_tot[p],col="red")
 }
+
+
+##### Question 1.c
+
+modelproject2 <-
+  "model
+{
+  # Definition des liens
+  for (i in 1:n)
+  {
+    ptox[i] = exp(a0 + theta*dose[i])/(1+exp(a0 + theta*dose[i]))
+    ntox[i] ~ dbinom(ptox[i],nb_ind)
+  }
+  # Definition des distributions a priori
+  p0 ~ dunif(0,pm)
+  gamma ~ dunif(dmin,dmax)
+  theta <- log((p0*(1-pm))/(pm*(1-p0)))/(dmin-gamma)
+  a0 <- log(pm/(1-pm))-log((p0*(1-pm))/(pm*(1-p0)))*gamma/(dmin-gamma)
+}"
+
+  dose_to_give <- c()
+  dose_init=c(0.5,3,5,5,5,5)
+  a0 = 1
+  ntox = c(0,1,1,0,1,2)
+  dose_tot = c(0.5,1,3,5,6)
+  nb_cohorte = 6
+  
+  
+  for(i in 1:(nb_cohorte-5)){
+    
+    data4jags <- list(
+      ntox = ntox[1:i] ,
+      dmin=0.5, dmax= 6 , pm=0.33,
+      dose = dose_init[1:i],
+      nb_ind = 3,
+      n = i
+    )
+    
+    
+    inits1 <- 
+      list(
+        list(p0=0.1,gamma=5),
+        list(p0=0.3,gamma=3.5),
+        list(p0=0.2,gamma=1)
+      )
+    
+    
+    
+    # Simulation des chaines de Markov
+    model <- jags.model(file =textConnection(modelproject2),data = data4jags, n.chains=3,init=inits1)
+    
+    # Phase de chauffe
+    update(model,3000)
+    
+    # Simulations monitorees
+    # Tirage de 2000 valeurs de theta
+    mcmc <- coda.samples(model,c("p0","gamma"),n.iter = 10000, thin = 3)
+    
+    # Estimation des parametres a posteriori
+    S=summary(mcmc)
+    plot(mcmc,trace = F, density = T)
+    # gelman.diag(mcmc)
+    #autocorr.plot(mcmc[[1]])
+    
+    mcmctot = as.data.frame(as.matrix(mcmc))
+    
+    
+    calc_p_tox <- function(mcmc,X){
+      theta <- log((mcmc$p0*(1-0.33))/(0.33*(1-mcmc$p0)))/(0.5-mcmc$gamma)
+      a0 <- log(0.33/(1-0.33))-log((mcmc$p0*(1-0.33))/(0.33*(1-mcmc$p0)))*mcmc$gamma/(0.5-mcmc$gamma)
+      return (exp(a0 + theta*X)/(1 +  exp(a0 + theta*X)))
+      
+    }
+    
+    ptox_tot = lapply(X = dose_tot, FUN = calc_p_tox, mcmc=mcmctot)
+    hist(ptox_tot[[4]])
+    
+    med_ptox=c()
+    for(j in 1:5){
+      med_ptox[j]<-median(ptox_tot[[j]])
+    }
+    
+    dose_to_give[i] <- dose_tot[sum(med_ptox<=0.33)] 
+    
+  }  
+  
+  
+
